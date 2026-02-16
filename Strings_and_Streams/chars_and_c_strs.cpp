b9cda5c4057ba8327d9bb507d38064274f7352be
Brian Wang <bzwang@wpi.edu>
2026-02-16 18:33:42 -0500

    Learned more about c-style strings

--- Strings_and_Streams/chars_and_c_strs.cpp
+++ Strings_and_Streams/chars_and_c_strs.cpp
@@ -6,6 +6,7 @@ g++ chars_and_c_strs.cpp -o run
 */
 
 #include <iostream>
+#include <cstring>
 
 int main(){
   // c-style string, null terminated
@@ -32,5 +33,26 @@ int main(){
   // may need to store as str if not enough bits
   const char* smiley{"😀"}; // U+1F600, requires 4 bytes to encode, 1 byte for null term
   std::cout << smiley << "\n";
+
+  // cstring library functions, in most cases, should not use c-style strings
+  std::cout << "Brian is " << strlen(c_str) << " letters\n";
+
+  const char* c_str2{"Andrew"};
+  if (strcmp(c_str2, c_str) > 0) std::cout << "\"Andrew\" comes before \"Brian\" alphabetically\n";
+
+  
+  // allocate enough mem or a shallow copy is made (ptr to same c_str)
+  char Source[50]{"Hello"};
+  char Dest[50];
+
+  // strcpy lacks safety checks for memory corruption
+  strcpy(Dest, Source);
+  std::cout << "Dest: " << Dest << "\n";
+
+
+  // strcat also lacks some safety checks against memory corruption
+  strcat(Dest, " World\n");
+  std::cout << "Concatenated: " << Dest;
+  std::cout << "Source remains unchanged: " << Source << "\n";
 
 }
