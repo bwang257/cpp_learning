55cbb4c9d7ddb573817becd0e3abda238491c3d1
Brian Wang <bzwang@wpi.edu>
2026-02-18 15:51:29 -0500

    Learned more about std::string functions

--- Strings_and_Streams/std_string.cpp
+++ Strings_and_Streams/std_string.cpp
@@ -2,11 +2,13 @@
 Deeper dive into std::string objects
 
 Compile using:
-g++ std_string.cpp -o run
+g++ -std=c++23 std_string.cpp -o run
 */
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 int main(){
   // std::string_literals creates a std::string directly
@@ -17,10 +19,18 @@ int main(){
   std::string str2{"Hi\0world"}; // iniitalize with c str constructor
   std::cout << str2 << "\n";
 
-  // concatenate using + operator
-  str2 += " world";
+  // concatenate using + operator (or append method) 
+  str2 += " "; // could have also done str.append(" ");
+
+  std::string str3{"World,Everyone,Friends"};
+  str2.append(str3, 6, 8); // starting from pos 6 of str3, append 8 characters
+  str2.append(3, '!');
   std::cout << str2 << "\n";
 
+  // append a range, c++23
+  std::vector<char> Characters{' ', ':', ')'};
+  str2.append_range(Characters);
+  std::cout << str2 << "\n";
 
   // copies of strings are deep copies
 
@@ -33,6 +43,35 @@ int main(){
   if (str.compare(str2) < 0 && str < str2){ // use compare or directly use < > 
     std::cout << "str comes before str2\n";
   }
+
+
+  // inserting content
+  std::string greeting{"Greetings  "};
+  std::string andr{"Andrew"};
+  greeting.insert(10, andr);
+  std::cout << greeting << "\n";
+
+  std::vector br_and{'B', 'r', 'i', 'a', 'n', ',', ' '};
+  greeting.insert_range(greeting.begin()+10, br_and);
+  std::cout << greeting << "\n";
+
+  // erasing content
+  // greeting.erase() // erase all chars
+  // greeting.erase(5) erase everything execept the first 5 chars
+  greeting.erase({}, 17); // erase first 17 chars
+  std::cout << greeting << "\n";
+
+  // replacing content
+  std::string greeting2{"Hello World!"};
+  std::string greeting3{greeting2};
+  // Starting from index 6, replace 5 characters, str to insert does not have be same length
+  greeting2.replace(6, 5, "other students");
+  std::cout << greeting2 << "\n";
+
+  // replace_with_range c++23
+  std::vector Chars{'E', 'v', 'e', 'r', 'y', 'o', 'n', 'e'};
+  greeting3.replace_with_range(greeting3.begin() + 6, greeting3.begin() + 11, Chars);
+  std::cout << greeting3 << "\n";
 
   // find returns position where first instance of a substring starts
   // rfind returns position where last instance of a substring starts
@@ -60,4 +99,20 @@ int main(){
   long double PiDouble{3.1415926535};
   std::string PiStr{std::to_string(PiDouble)};
   std::cout << PiStr << "\n";
+
+  // able to iterate over string (for const char& c : str)...
+  // allows use of strings as ranges
+  std::string name{"Brian"};
+  std::reverse(name.begin(), name.end());
+
+  std::string my_sentence{"The quick brown fox jumped over the log."};
+
+  std::cout << "The string has " << std::ranges::count(my_sentence, ' ') + 1 << " word\n";
+
+
+  // std::string is alias for std::basic_string<char>
+
+  // L is literal for creating wide char str
+  std::wstring w_str{L"Hello\n"}; // std::basic_string<wchar_t>
+  std::wcout << w_str; // need wcout, cout is intended for narrow characters
 }
