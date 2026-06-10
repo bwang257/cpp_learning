591e5d1b30fca504c7f7d8e643d4f066a0f41bff
Brian Wang <bzwang@wpi.edu>
2026-06-10 08:15:56 -0400

    reviewed and updated move semantics

--- MemoryManagement/move_semantics.cpp
+++ MemoryManagement/move_semantics.cpp
@@ -1,36 +1,28 @@
 /*
 Move Semantics in C++
-*/
-/**
-Often the objects we create must store otehr objects in them.
-This can make deep copying expensive. 
-*/
 
-/**
-Move semantics are simply a performance optimization
+Deep copying can be expensive - performance optimization occurs 
+using move semantics
+
 The compiler can still fulfill move requests by using the type's
 copy semantics. Resource Moved{std::move(A)} will call the Resource copy constructor
+
+g++ move_semantics.cpp -o run
 */
-
-
 
 #include <iostream>
 #include <memory>
 #include <vector>
 
-using std::cout;
-using std::endl;
-using std::vector;
-
 
 struct SubResource {
   SubResource(){
-    cout << "Creating subresource" << endl;
+    std::cout << "Creating subresource" << '\n';
   };
 
   // copy constructor, l-value
   SubResource(const SubResource& source){
-    cout << "Copying subresource (expensive!)" << endl;
+    std::cout << "Copying subresource (expensive!)" << '\n';
   }
 };
 
@@ -44,12 +36,12 @@ struct Resource {
   // }
 
   Resource(){
-    cout << "Creating resource" << endl;
+    std::cout << "Creating resource" << '\n';
   }
   
   // copy constructor -  when we still want to use the original object, l value
   Resource(const Resource& source) : sub{std::make_unique<SubResource>(*source.sub) } {
-    cout << "Copying Resource" << endl;
+    std::cout << "Copying Resource" << '\n';
   }
 
   // move constructor
@@ -60,19 +52,19 @@ struct Resource {
   shallow copy, transfer ownership of the subresources to the shallow copy.
   modifies original object
 
-  // takes in rvalue reference
+  takes in rvalue reference
   */
   Resource(Resource&& source) : sub{std::move(source.sub)} {
-    cout << "Moving Resource\n";
+    std::cout << "Moving Resource\n";
   }
 
   // move assignment operator, rvalue reference
   Resource& operator=(Resource&& source){
     if (&source == this){
-      cout << "Same object, skipping move assignmnet\n";
+      std::cout << "Same object, skipping move assignmnet\n";
       return *this;
     }
-    cout << "Moving by assignment\n";
+    std::cout << "Moving by assignment\n";
     sub = std::move(source.sub);
     return *this;
   }
@@ -81,30 +73,31 @@ struct Resource {
 };
 
 int main(){
-  cout << "Resource A:\n";
+  std::cout << "Resource A:\n";
   Resource A;
 
   // Unnecessary copying when moving Resource into a vector
-  cout << "\nMoving Resource A into a vector:\n";
-  vector<Resource> vect;
-  vect.push_back(A);
+  std::cout << "\nMoving Resource A into a vector:\n";
+  std::vector<Resource> vect;
+  vect.push_back(A); // note copying subresource first
 
-  // std::move() signals to the compiler that its safe to use ur type's move semantiucs
-  cout << "\nResource B:\n";
+  // std::move() signals to the compiler that its safe to use your type's move semantiucs
+  std::cout << "\nResource B:\n";
   Resource B{std::move(A)};
 
   if (!A.sub.get()){
-    cout << "Resource A no longer has its subresource\n";
+    std::cout << "Resource A no longer has its subresource\n";
   }
 
+  std::cout << "\nResource C\n";
   Resource C;
-  cout << "\nMoving B to C\n";
+  std::cout << "\nMoving B to C\n";
   C = std::move(B);
   if (!B.sub.get()){
-    cout<< "Resource B no longer has its subresource\n";
+    std::cout<< "Resource B no longer has its subresource\n";
   }
 
-  cout << "\nMoving C to C\n";
+  std::cout << "\nMoving C to C\n";
   C = std::move(C);
 
 }
