71dd89fbc582cdd350e73756c8dbce00872fb900
Brian Wang <bzwang@wpi.edu>
2026-06-20 22:13:44 -0400

    Slight refactor, reviewed stack heap concepts

--- MemoryManagement/stack_heap.cpp
+++ MemoryManagement/stack_heap.cpp
@@ -13,10 +13,10 @@ public:
         std::cout << "Creating myObject!\n";
     }
     myObject(std::string entered_name) : name(entered_name){
-        std::cout << "Creating object with name " << name << std::endl;
+        std::cout << "Creating object with name " << name << '\n';
     }
     ~myObject(){
-        std::cout << "Destroying myObject " << name << std::endl;
+        std::cout << "Destroying myObject " << name << '\n';
     }
 };
 
@@ -32,19 +32,22 @@ myObject testMoving();
 int main() {
   std::cout << "Calling testStack\n";
   testStack();
+  std::cout << '\n';
+
   std::cout << "Calling testMoving\n";
   myObject test = testMoving();
   test.name = "MovedObject";
-  std::cout << test.name << std::endl;
+  std::cout << "Returned\n";
+  std::cout << '\n';
 
   std::cout << "Creating object on the heap\n";
   // memory on stack used to store pointer to heap object
   myObject* heapObject{new myObject{createJeff()}};
-  std::cout << heapObject->name << std::endl;
+  std::cout << heapObject->name << '\n';
   std::cout << "Deleting object on the heap\n";
   delete heapObject;
   
-  std::cout << "Program Ending\n";
+  std::cout << "\nProgram Ending\n";
 }
 
 void testStack(){
@@ -60,5 +63,6 @@ myObject testMoving(){
     test moving the object, object is not deleted until the end
     */
     myObject obj;
+    std::cout << "Returning\n";
     return obj;
 }
