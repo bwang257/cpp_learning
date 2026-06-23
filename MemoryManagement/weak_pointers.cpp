2827fbf98576949218a893991c0876e7837650c4
Brian Wang <bzwang@wpi.edu>
2026-06-23 19:47:09 -0400

    Revised and refactored understanding of weak ptrs

--- MemoryManagement/weak_pointers.cpp
+++ MemoryManagement/weak_pointers.cpp
@@ -1,35 +1,49 @@
 /* 
-Introduction to weak pointers
+Introduction to weak pointers - smart ptrs without ownership
+Does not include entire API: see more https://en.cppreference.com/cpp/memory/weak_ptr
+
+g++ weak_pointers.cpp -o run
 */
 
 #include <memory>
 #include <iostream>
-
-using std::cout;
-using std::endl;
+#include <cassert>
 
 /*
-Key Advantage: ability to know whether a ptr has expired. crucial layer of awareness in larger projects
+Useful when an object only needs to be accessed only if it exists, and it may be deleted any time by
+someone else. Can also be used to break reference cycles
 */
 
 int main() {
     auto SharedPtr { std::make_shared<int>(42) };
-    std::weak_ptr<int> WeakPtr{SharedPtr};
+    std::weak_ptr<int> WeakPtr{SharedPtr}; 
 
-    // expired() and use_cout() methods
+    std::cout << "Size: " << sizeof(WeakPtr) << " bytes\n";
+    
+    // can't directly dereference, need to convert to shared_ptr with lock()
+    // lock attempts to create a temporary std::shared_ptr
+    auto SharedPtr2{WeakPtr.lock()};
+    std::cout << "Weak ptr currently points to " *SharedPtr2 << '\n';
+
+    // expired() and use_count() methods
     SharedPtr.reset();
-    cout<< "SharedPtr has been reset" << endl;
-    if (WeakPtr.expired()) cout << "The underlying resource has be deallocated" << endl;
-    cout << "The underlying resource has " << WeakPtr.use_count() << " owners\n" << endl;
+    std::cout<< "\nSharedPtr has been reset" << '\n';
+    if (WeakPtr.expired()) std::cout << "The underlying resource has be deallocated" << '\n';
+    std::cout << "The underlying resource has " << WeakPtr.use_count() << " owners\n" << '\n';
+
+
+    // can't convert weak ptr to bool
+    std::shared_ptr<int> SharedPtr3{WeakPtr.lock()};
+    assert(SharedPtr3);
 
     // access underlying object of a weakptr under conditions that we do not know whether
     // the underlying resource has expired. This is referred to as locking the weak ptr
+
     if (std::shared_ptr<int> LockedPtr{WeakPtr.lock()}) {
-        std::cout << "The number is " << *LockedPtr << endl;
+        std::cout << "The number is " << *LockedPtr << '\n';
     } else {
-        std::cout << "\nThe pointer has expired" << endl;
+        std::cout << "\nThe pointer has expired" << '\n';
     }
-    return 0;
 }
 
 
